2d40b57034c435a2f3dd459bf5124c2c7734dedf
LukeOfM <lemayer214@gmail.com>
2024-09-25 22:39:35 -0400

    Fixed profile photo bug

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <Image
           borderRadius="full"
           boxSize="250px"
-          src="src\assets\profile-pic-square.jpg"
+          src="https://github.com/luke-mayer/portfolio-site/blob/main/src/assets/profile-pic.jpg"
           alt="Luke-Photo"
         />
         <Heading as="h2" size="2xl" align="center">
